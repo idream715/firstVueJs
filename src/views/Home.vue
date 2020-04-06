7b5d5a3ec61b6c431f0725f7091aae78b105d8a7
idream715 <t.tangburanakul5170@gmail.com>
2020-04-06 18:09:33 +0700

    work 3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h2>ค้นหาศูนย์แต่ละจังหวัด</h2>
+    <h2>{{lastMember}}</h2>
+
+    <p>ชื่อของคุณ : {{ msgHome }}</p>
+
+    <input v-model="msgHome" placeholder="ชื่อจริง">
+    <HelloWorld @lastMem="lastMem" :msg="msgHome"/>
   </div>
 </template>
 
@@ -13,6 +20,17 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      msgHome:"",
+      lastMember:""
+    }
+  },
+  methods:{
+    lastMem(val){
+      this.lastMember = val
+    }
   }
 }
 </script>
