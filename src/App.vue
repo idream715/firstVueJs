13eda42c01f779fa1ab2a3a76525f96ef5cc5447
idream715 <t.tangburanakul5170@gmail.com>
2020-04-08 15:18:04 +0700

    work 4

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,14 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/result">Result</router-link>
     </div>
    
   
     <router-view/>
   </div>
 </template>
-
-<script>
-  export default {
-   
-  }
-</script>
 
 <style>
 #app {
---
+++ src/views/Result.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="Result">
+        <div class="row">
+            <img alt="proflie" src="../assets/6001032.jpg">
+            <h4>ชื่อ : {{name}}</h4>
+            <h4>ฉายา : {{lastName}}</h4>
+            <h4>ศูนย์ : {{lastMem.addressName}}</h4>
+            <h4>จังหวัด : {{lastMem.addressLv2}}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    methods:{
+    },
+    computed:{
+        name(){
+            let getInput = this.$store.getters.getInput
+            if(getInput){
+                return  `พระ${getInput}`
+            }else{
+                return  this.$store.getters.getName
+            }
+        },
+        lastName(){
+            return this.$store.getters.getLastName
+        },
+        lastMem(){
+            return this.$store.getters.getLastMem
+        }
+    }
+}
+</script>
+
+<style scoped>
+img {
+    width : 200px;
+    float : center;
+}
+h4 {
+    text-align: center;
+    margin: 0 0 15 100px;
+}
+</style>
